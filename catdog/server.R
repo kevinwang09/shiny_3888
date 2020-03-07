929b2627f5334eb94efe97db9a4c2966f9b22020
Kevin Wang <kevin.wang@sydney.edu.au>
2020-03-07 15:40:25 -0500

    add reactive function

--- catdog/server.R
+++ catdog/server.R
@@ -1,5 +1,11 @@
 shinyServer(function(input, output) {
+    tmp_file = shiny::reactive({
+        download.file("https://cdn.mos.cms.futurecdn.net/vChK6pTy3vN3KbYZ7UU7k3.jpg",
+                      destfile = paste0(getwd(), "tmp.jpg"))
+        paste0(getwd(), "tmp.jpg")
+    })
+    
     output$pred_table = renderDataTable(
-        pred_cats_dogs(model = model, file_name = paste0(getwd(), "tmp.jpg"))
+        pred_cats_dogs(model = model, file_name = tmp_file())
     )
 })
