48f972df57c9075bf7809496feafab4864525098
Kevin Wang <kevin.wang@sydney.edu.au>
2020-03-08 00:02:48 -0500

    get ready for bomrang app

---
+++ bomrang_3888/ui.R
@@ -0,0 +1,15 @@
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+        )
+    )
+))
