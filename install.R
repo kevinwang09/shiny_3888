f62ca673a76a7b4a0dc6053b3a4046b86f53e8bb
Kevin Wang <kevin.wang@sydney.edu.au>
2020-03-09 15:56:53 -0400

    add lecture slides

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-install.packages(c("devtools", "shiny", "tidyverse", "DT"))
+install.packages(c("devtools", "shiny", "tidyverse", "DT", "forecast"))
 devtools::install_github("rstudio/keras")
 library(keras)
 install_keras()
